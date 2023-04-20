bf9612bf973d0489ffbb3ad58902f16742e380d2
Phillip Rak <rak.phillip@gmail.com>
2023-04-20 14:29:50 -0700

    Create a button component for installing extensions
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ pkg/rancher-desktop/components/ExtensionDetailsButtonInstall.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import Vue from 'vue';
+
+import LoadingIndicator from '@pkg/components/LoadingIndicator.vue';
+
+export default Vue.extend({
+  name:       'extension-details-button-install',
+  components: { LoadingIndicator },
+  data() {
+    return {
+      loading:     false,
+      isInstalled: null,
+    };
+  },
+  computed: {
+    installationAction(): string {
+      return this.isInstalled ? 'uninstall' : 'install';
+    },
+    buttonLabel(): string {
+      if (this.loading) {
+        return this.isInstalled ? this.t('marketplace.sidebar.uninstallButton.loading') : this.t('marketplace.sidebar.installButton.loading');
+      } else {
+        return this.isInstalled ? this.t('marketplace.sidebar.uninstallButton.label') : this.t('marketplace.sidebar.installButton.label');
+      }
+    },
+  },
+});
+</script>
+
+<template>
+  <button
+    data-test="button-install"
+    class="btn btn-xs role-primary"
+    :disabled="loading"
+    @click="appInstallation(installationAction)"
+  >
+    <span v-if="loading" name="loading" :is-loading="loading">
+      <loading-indicator>{{ buttonLabel }}</loading-indicator>
+    </span>
+
+    <span v-if="!loading">{{ buttonLabel }}</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  .btn-xs {
+    min-height: 2.25rem;
+    max-height: 2.25rem;
+    line-height: 0.25rem;
+  }
+</style>
